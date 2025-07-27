7cc526e170fb066a2eed2b4b67504c5f226c26ae
Aryan Srivastava <114677161+AryanSrivastava15@users.noreply.github.com>
2025-07-27 22:02:29 +0530

    SIgnUp Setup Completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,17 @@ dotenv.config();
 // import "dotenv/config";
 
 import authRoutes from"./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
+
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
