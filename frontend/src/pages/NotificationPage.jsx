afb08e884c88fea182b1becb94fe060f7cf21f94
Aryan Srivastava <114677161+AryanSrivastava15@users.noreply.github.com>
2025-08-02 15:35:03 +0530

    FrontEnd Setup Completed

---
+++ frontend/src/pages/NotificationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotificationPage = () => {
+  return (
+    <div>
+      Notifications
+    </div>
+  )
+}
+
+export default NotificationPage
---
+++ frontend/src/pages/OnboardingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OnboardingPage = () => {
+  return (
+    <div>
+      OnBoarding
+    </div>
+  )
+}
+
+export default OnboardingPage
