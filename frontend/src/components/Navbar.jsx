3da5ccddf95e53aac7d8fdd8083801b2a9936c7d
Aryan Srivastava <114677161+AryanSrivastava15@users.noreply.github.com>
2025-08-08 23:43:36 +0530

    Custom hook update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import { Link, useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser.js"
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { logout } from "../lib/api.js";
 import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector.jsx";
+import useLogout from "../hooks/useLogout.js";
 
 const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
   const isChatPage = location.pathname?.startsWith("/chat");
 
-  const queryClient = useQueryClient();
+//   const queryClient = useQueryClient();
 
-  const { mutate: logoutMutation } = useMutation({
-    mutationFn: logout,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"]} )
-  })
+//   const { mutate: logoutMutation } = useMutation({
+//     mutationFn: logout,
+//     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"]} )
+//   })
+
+  const { logoutMutation } = useLogout()
   
   return (
     <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
